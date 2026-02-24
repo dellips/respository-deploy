2ee20f2758a154371b2b8a3791e810eebd420e90
dellips <dellianasalsabilap3@gmail.com>
2026-02-24 14:48:34 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+
+const app = express();
+
+// middleware
+app.use((req, res, next) => {
+    console.log(`Request lewat sini ${req.path}`);
+    if(false){
+        next(new Error('Not Authorized'));
+        return;
+    }
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello Delli! ^^');
+});
+
+// using path parameters
+app.get('/say/:greeting', (req, res) => {
+    const { greeting } = req.params;
+    res.send(greeting);
+});
+
+// error handling middleware
+app.use((err, req, res, next) => {
+    res.send('Error Occurred');
+})
+
+app.listen(3000);
